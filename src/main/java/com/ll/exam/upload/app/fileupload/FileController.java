6a16787236f3bbcf1d3d1c41b98ad4ac4a70bfd0
ddmkim94 <ddmkim94@gmail.com>
2022-09-05 15:53:54 +0900

    파일 업로드 테스트1

---
+++ src/main/java/com/ll/exam/upload/app/fileupload/FileController.java
@@ -0,0 +1,33 @@
+package com.ll.exam.upload.app.fileupload;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/upload")
+public class FileController {
+
+    @Value("${custom.genFileDirPath}")
+    private String genFileDirPath;
+
+    @PostMapping
+    @ResponseBody
+    public String upload(@RequestParam("img1") MultipartFile img1) {
+        try {
+            img1.transferTo(new File(genFileDirPath + "/1.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "업로드 완료!";
+    }
+}
--- src/main/java/com/ll/exam/upload/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/upload/app/home/controller/HomeController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
     @RequestMapping("/")
     public String main() {
         return "home/main";
+    }
+
+    @RequestMapping("/test/upload")
+    public String uploadForm() {
+        return "home/test/upload";
     }
 }
