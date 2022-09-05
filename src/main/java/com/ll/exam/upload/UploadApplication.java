4a00b9bd13dc0c2496dac80860e74266f2a5aca2
ddmkim94 <ddmkim94@gmail.com>
2022-09-05 14:54:32 +0900

    프로젝트 추가 설정

--- src/main/java/com/ll/exam/upload/UploadApplication.java
+++ src/main/java/com/ll/exam/upload/UploadApplication.java
@@ -2,8 +2,10 @@ package com.ll.exam.upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UploadApplication {
 
 	public static void main(String[] args) {
