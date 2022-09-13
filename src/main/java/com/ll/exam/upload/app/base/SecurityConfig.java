24d8255996f417065f904f7ded10549f6411aa3c
ddmkim94 <ddmkim94@gmail.com>
2022-09-13 15:15:58 +0900

    회원가입 테스트 케이스 & 로그인 url을 /member/login 으로

--- src/main/java/com/ll/exam/upload/app/base/SecurityConfig.java
+++ src/main/java/com/ll/exam/upload/app/base/SecurityConfig.java
@@ -16,7 +16,11 @@ public class SecurityConfig {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/**")
-                .permitAll();
+                .permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/member/login") // GET
+                .loginProcessingUrl("/member/login"); // POST
         return http.build();
     }
 
--- src/main/java/com/ll/exam/upload/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/upload/app/member/service/MemberService.java
@@ -33,16 +33,20 @@ public class MemberService implements UserDetailsService {
     }
 
     public Member join(String username, String password, String email, MultipartFile profileImg) {
-        String profileImgRelPath = "member/" + UUID.randomUUID().toString() + ".png";
-        File profileImgFile = new File(genFileDirPath + "/" + profileImgRelPath);
+        String profileImgDirName = "member";
+        String fileName = UUID.randomUUID().toString() + ".png";
+        String profileImgDirPath = genFileDirPath + "/" + profileImgDirName;
+        String profileImgFilePath = profileImgDirPath + "/" + fileName;
 
-        profileImgFile.mkdirs(); // 관련된 폴더가 혹시나 없다면 만들어준다.
+        new File(profileImgDirPath).mkdirs(); // 폴더가 혹시나 없다면 만들어준다.
 
         try {
-            profileImg.transferTo(profileImgFile);
+            profileImg.transferTo(new File(profileImgFilePath));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        String profileImgRelPath = profileImgDirName + "/" + fileName;
 
         Member member = Member.builder()
                 .username(username)
@@ -85,5 +89,12 @@ public class MemberService implements UserDetailsService {
 
     public long count() {
         return memberRepository.count();
+    }
+
+    public void removeProfileImg(Member member) {
+        member.removeProfileImgOnStorage(); // 파일삭제
+        member.setProfileImg(null);
+
+        memberRepository.save(member);
     }
 }
