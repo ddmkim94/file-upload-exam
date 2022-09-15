b9b553602d3a9a8e2db3f9681e63825991428b6e
ddmkim94 <ddmkim94@gmail.com>
2022-09-15 14:52:51 +0900

    main.html에 현재 로그인한 회원의 이름과 프로필 사진 보여주기
    현재 방법은 매우 비효율적인 방법이다.. 각 화면마다 동일한 코드를 전부 작성해야하는 번거로움을 가진 코드..

--- src/main/java/com/ll/exam/upload/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/upload/app/home/controller/HomeController.java
@@ -1,16 +1,37 @@
 package com.ll.exam.upload.app.home.controller;
 
 
+import com.ll.exam.upload.app.member.entity.Member;
+import com.ll.exam.upload.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final MemberService memberService;
+
     @RequestMapping("/")
-    public String main() {
+    public String main(Principal principal, Model model) {
+        Member loginedMember = null;
+        String profile = "";
+
+        if(principal != null && principal.getName() != null) {
+            loginedMember = memberService.getMemberByUsername(principal.getName());
+        }
+
+        if(loginedMember != null) {
+            profile = loginedMember.getProfileImgUrl();
+        }
+
+        model.addAttribute("loginedMember", loginedMember);
+        model.addAttribute("profile", profile);
+
         return "home/main";
     }
 
