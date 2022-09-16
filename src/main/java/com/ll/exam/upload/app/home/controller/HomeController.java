9db06ddf43bcb675a1546a9d1a59b00a71579ab0
ddmkim94 <ddmkim94@gmail.com>
2022-09-16 14:17:36 +0900

    모든 화면마다 작성해 주었던 username, profile 사진 가져오던 로직 제거
    authentication.principal 로 불완전하게나마 해결!!

--- src/main/java/com/ll/exam/upload/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/upload/app/home/controller/HomeController.java
@@ -18,40 +18,12 @@ public class HomeController {
     private final MemberService memberService;
 
     @RequestMapping("/")
-    public String main(Principal principal, Model model) {
-        Member loginedMember = null;
-        String profile = "";
-
-        if(principal != null && principal.getName() != null) {
-            loginedMember = memberService.getMemberByUsername(principal.getName());
-        }
-
-        if(loginedMember != null) {
-            profile = loginedMember.getProfileImgUrl();
-        }
-
-        model.addAttribute("loginedMember", loginedMember);
-        model.addAttribute("profile", profile);
-
+    public String main() {
         return "home/main";
     }
 
     @GetMapping("/about")
-    public String showAbout(Principal principal, Model model) {
-        Member loginedMember = null;
-        String loginedMemberProfileImgUrl = null;
-
-        if (principal != null && principal.getName() != null) {
-            loginedMember = memberService.getMemberByUsername(principal.getName());
-        }
-
-        if (loginedMember != null) {
-            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
-        }
-
-        model.addAttribute("loginedMember", loginedMember);
-        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
-
+    public String showAbout() {
         return "home/about";
     }
 }
