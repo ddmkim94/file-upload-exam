9fdbcfcd7db655a80198e8f22b3f6b47c6ad2830
ddmkim94 <ddmkim94@gmail.com>
2022-09-16 14:08:20 +0900

    about 페이지 추가

--- src/main/java/com/ll/exam/upload/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/upload/app/home/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.ll.exam.upload.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -35,8 +36,22 @@ public class HomeController {
         return "home/main";
     }
 
-    @RequestMapping("/test/upload")
-    public String uploadForm() {
-        return "home/test/upload";
+    @GetMapping("/about")
+    public String showAbout(Principal principal, Model model) {
+        Member loginedMember = null;
+        String loginedMemberProfileImgUrl = null;
+
+        if (principal != null && principal.getName() != null) {
+            loginedMember = memberService.getMemberByUsername(principal.getName());
+        }
+
+        if (loginedMember != null) {
+            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
+        }
+
+        model.addAttribute("loginedMember", loginedMember);
+        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
+
+        return "home/about";
     }
 }
