3f7c52e5cbedb5d8f143afafd44baa19ca11de9a
ddmkim94 <ddmkim94@gmail.com>
2022-09-16 15:08:30 +0900

    MemberContext를 통해 profile.html에서 정보 가져오기
    email 필드 추가

--- src/main/java/com/ll/exam/upload/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/upload/app/member/controller/MemberController.java
@@ -2,9 +2,11 @@ package com.ll.exam.upload.app.member.controller;
 
 import com.ll.exam.upload.app.member.entity.Member;
 import com.ll.exam.upload.app.member.service.MemberService;
+import com.ll.exam.upload.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -61,11 +63,8 @@ public class MemberController {
     // 현재 로그인 된 유저만 접근이 가능하다!
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/profile")
-    public String showProfile(Principal principal, Model model) {
-        Member loginedMember = memberService.getMemberByUsername(principal.getName());
-
-        model.addAttribute("loginedMember", loginedMember);
-
+    public String showProfile(@AuthenticationPrincipal MemberContext memberContext, Model model) {
+        model.addAttribute("memberContext", memberContext);
         return "member/profile";
     }
 }
