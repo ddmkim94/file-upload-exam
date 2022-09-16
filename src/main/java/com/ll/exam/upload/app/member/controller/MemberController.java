10508e5e7be16fb6e49ed0850d4733b7f96db7e1
ddmkim94 <ddmkim94@gmail.com>
2022-09-16 15:12:50 +0900

    #authentication.principal를 이용해서 profile.html 렌더링
    Model에 담아서 가져올 필요가 없음, 타임리프에서 바로 접근이 가능

--- src/main/java/com/ll/exam/upload/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/upload/app/member/controller/MemberController.java
@@ -63,8 +63,7 @@ public class MemberController {
     // 현재 로그인 된 유저만 접근이 가능하다!
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/profile")
-    public String showProfile(@AuthenticationPrincipal MemberContext memberContext, Model model) {
-        model.addAttribute("memberContext", memberContext);
+    public String showProfile() {
         return "member/profile";
     }
 }
